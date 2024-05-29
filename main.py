34cfbf076cb7e39663c00efa49953cf18b651eb9
IvanNugiban <87913973+IvanNugiban@users.noreply.github.com>
2024-05-29 12:55:01 +0300

    Added config.json
    Now all parameters such as mode or items are in config.json, no need for user to write them in input each time when program launches.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, argparse, multiprocessing
+import os, argparse, multiprocessing, json
 from src import audio
 from src.video import Video
 from dotenv import load_dotenv
@@ -8,35 +8,17 @@ load_dotenv()
 session_id = os.getenv("session_id")
 output_path = "output"
 
-def choose_mode(args):
+def choose_mode(data):
 
-    args.s = format_song(args.s)
+    if data['mode'] == 'manual':
 
-    # -m = manual, -a = automatic
-    if args.mode == 'm':
-
-        items = []
-
-        while(True):
-            video_path = input(str("Specify the path to the video: (leave empty to continue)" ))
-
-            if video_path == '':
-                break
-        
-            # Remove quotes (because when you copy path in Windows they are automaticly copied, that's annoing to always remove them manualy)
-            video_path = video_path.replace('"', '')
-
-            video_text = input(str("Specify video text:"))
-
-            items.append({'path' : video_path.strip(), 'text' : video_text.strip()})
-
-        if len(items) == 0:
+        if len(data['items']) == 0:
             print('Please, provide at least 1 video!')
             return    
 
-        create_videos(items, args.s, args.skip)
+        create_videos(data)
 
-    elif args.mode == 'a':
+    elif data['mode'] == 'auto':
         pass
 
     else:
@@ -69,29 +51,29 @@ def create_video(item, voice_path, subtitles, index):
     return video_path
     
 
-def create_videos(videos, song : str, skip):
+def create_videos(data):
 
     subtitles_audio = []
     subtitles = []
 
-    for i, item in enumerate(videos):
+    for i, item in enumerate(data['items']):
         # Create subtitles track
         voice_path = audio.tts(session_id, i, "en_us_010", item['text'], os.path.join(output_path, 'output' + str(i) + '.mp3'))
 
         subtitles_audio.append(voice_path)
 
         # Create subtitles
-        subtitles.append(audio.generate_subtitles(voice_path, output_path, skip))
+        subtitles.append(audio.generate_subtitles(voice_path, output_path, data['skip']))
     
     # Ask user to mannualy continue, because he might want to edit srt file
-    if not skip:
+    if not data['skip']:
         input('Press any key to continue')
 
-    args = [(video, subtitles_audio[index], subtitles[index], index) for  index, video in enumerate(videos)]
+    args = [(video, subtitles_audio[index], subtitles[index], index) for  index, video in enumerate(data['items'])]
 
     
     # Determine the number of worker processes to use
-    num_workers = min(len(videos), multiprocessing.cpu_count())
+    num_workers = min(len(data['items']), multiprocessing.cpu_count())
     
     # Create a pool of worker processes
     with multiprocessing.Pool(processes=num_workers) as pool:
@@ -101,25 +83,17 @@ def create_videos(videos, song : str, skip):
     combined_video = combine_videos([Video(result) for  result in results])
 
      # Create song track (if needed)
-    if song != '':
-       combined_video.add_audio(song, 0.1)
+    if data['song'] != '':
+       combined_video.add_audio(format_song(data['song']), 0.1)
 
     combined_video.save(os.path.join(output_path, "output.mp4"), True)
 
 def main():
 
-    parser = argparse.ArgumentParser('AutoShorts')
-    # There are 2 modes - manual and automatic. In manual you have to path  and text for the video.
-    # In automatic mode you have to just provide video url
-    parser.add_argument('mode', default='m', choices=['m', 'a'], help='Parser mode')
-    # Song is an optional argument, it is either pat to file or to folder
-    parser.add_argument('--s', '--path', required=False, default='')
-    # Argument for automatic skip of requesting user to press any key 
-    parser.add_argument('-a', '--skip', action='store_true', help='Automatic skip flag')
-    args = parser.parse_args()
+    with open('config.json') as config:
+        data = json.load(config)
 
     # Check if our folder for output exists, if not - create
-
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
@@ -130,7 +104,7 @@ def main():
         if os.path.isfile(file_path):
             os.remove(file_path)
 
-    choose_mode(args)
+    choose_mode(data)
 
 if __name__ == '__main__':
     main()
